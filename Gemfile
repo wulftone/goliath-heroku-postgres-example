40919fc6f532ce65bc4ecd83233338874a24b053
trevor <trevor@mint>
2012-05-06 00:32:36 -0700

    first

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source :gemcutter
+
+gem 'goliath', :git => 'git://github.com/postrank-labs/goliath.git'
+gem 'em-postgresql-adapter', :git => 'git://github.com/leftbee/em-postgresql-adapter.git'
+gem 'em-synchrony', :git => 'git://github.com/igrigorik/em-synchrony.git'
+gem 'yajl-ruby'
---
+++ hello.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require 'goliath'
+require 'em-synchrony/activerecord'
+require 'yajl'
+
+db = YAML.load_file('config/database.yml')['development']
+ActiveRecord::Base.establish_connection(db)
+
+class User < ActiveRecord::Base
+end
+
+class HelloWorld < Goliath::API
+  def response(env)
+    page = "<h1>#{Goliath.env} hello world!</h1><form action='hello_world' method='post'>Id:<input type='text' name='id' /><input type='submit' /></form>"
+    [200, {}, page]
+  end
+end
+
+class PostHelloWorld < Goliath::API
+  use Goliath::Rack::Params
+
+  def response(env)
+    user = User.find(params['id'])
+    [200, {}, "hello post world! #{params}, #{user.first_name}"]
+  end
+end
+
+class RackRoutes < Goliath::API
+  get  "/hello_world", HelloWorld
+  post "/hello_world", PostHelloWorld
+end
