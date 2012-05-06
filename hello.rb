00d736a33be31f0ad5cffb3f3691c2de191cec35
trevor <trevor@mint>
2012-05-06 01:47:15 -0700

    More db tests

--- hello.rb
+++ hello.rb
@@ -8,20 +8,20 @@ require 'yajl'
 # require 'activerecord'
 require 'uri'
 
-db = URI.parse(ENV['DATABASE_URL'] || 'http://localhost')
-if db.scheme == 'postgres'
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-  )
-else
-  db = YAML.load(ERB.new(File.read('config/database.yml')).result)['development']
+# db = URI.parse(ENV['DATABASE_URL'] || 'http://localhost')
+# if db.scheme == 'postgres'
+#   ActiveRecord::Base.establish_connection(
+#     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#     :host     => db.host,
+#     :username => db.user,
+#     :password => db.password,
+#     :database => db.path[1..-1],
+#     :encoding => 'utf8'
+#   )
+# else
+  db = YAML.load(ERB.new(File.read('config/database.yml')).result)['production']
   ActiveRecord::Base.establish_connection(db)
-end
+# end
 
 class User < ActiveRecord::Base
 end
